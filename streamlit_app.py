3a2da125931c18c3180f87dccfd72be10782ad93
knntiptri <jienanrr@gmail.com>
2025-05-27 11:45:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.write("From roaring engines to silent strategy rooms—
 Drive to Survive takes you to the heart of Formula 1 drama.
 Witness how dreams are built—and sometimes broken—in seconds.")
 st.write("Welcome to Drive to Survive—where every second counts.")
+st.image("614ae42590950a1da71afa8354be3a83.jpg", width=200)
