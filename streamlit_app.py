010af4b7407aa1ca8920cbc4f8767ab2130a791d
knntiptri <jienanrr@gmail.com>
2025-05-27 12:02:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("Formula 1:Drive to Survive")
-st.write("From roaring engines to silent strategy rooms Drive to Survive takes you to the heart of Formula 1 drama Witness how dreams are built and sometimes broken in seconds")
+st.write("From roaring engines to silent strategy rooms")
+st.write("Drive to Survive takes you to the heart of Formula 1 drama Witness how dreams are built and sometimes broken in seconds")
 st.write("Welcome to Drive to Survive where every second counts")
-st.image("614ae42590950a1da71afa8354be3a83.jpg", width=200)
+st.image("614ae42590950a1da71afa8354be3a83.jpg", width=500)
